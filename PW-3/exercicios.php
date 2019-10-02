f06c495439e33e364837bf5b8fa08518c3aeb2b2
edsonrgr <digabum@gmail.com>
2019-10-01 22:03:58 -0300

    exercicios em php

---
+++ PW-3/exercicios.php
@@ -0,0 +1,85 @@
+<?php
+/*class MinhaClass
+{ 
+	function valorNaTela()	{
+		$valor = -4;
+		if($valor> 0){
+			echo "Valor positivo";
+		}
+		else if ($valor < 0 ){
+			echo "Valor negativo";
+		}
+		else if ($valor == 0){
+			echo "o valor é zero";
+		}
+	}
+}
+$obj= new MinhaClass; 
+$obj->valorNaTela();
+
+*/
+
+
+/*class OutraClass {
+	function tabuada() {
+		$n = 22;
+		for($x = 0;$x <= 10;$x++){
+			$result = $n * $x;
+			echo " $n x $x = $result <br>";
+		}
+	}
+}
+
+$obj= new OutraClass;
+$obj->tabuada();
+
+*/
+
+
+
+/*class MaisUmaClass {
+
+	function numPares(){
+		for($i=0;$i<=200;$i++){
+		$par = $i%2;
+			if ($par == 0){
+				echo "$i <br> ";
+			}
+		; 
+
+		}
+	}
+} 
+
+$obj= new MaisUmaClass;
+$obj->numPares();
+*/
+
+/*
+<?php
+class Usuario{    isso é uma classe
+
+public $nome;    essas são as propriedades
+public $cpf;
+public $endereco;
+
+
+
+	function preparaUsuario(){ isso é um metodo
+		$this->nome = "xxxxxxxxxxx";
+		$this->cpf = "99999999999";
+		$this->endereco = "Rua Fulano de Tal número 0 apt 999";
+	}
+} ?>
+
+
+
+
+*/
+
+
+
+
+
+
+?>
